e94e4801220c4f0d7a04ccb9ed50df454374138c
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-06 18:51:14 +0400

    updated

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -1,15 +1,16 @@
 package school.mjc.stage0.module3.task2;
 
 public class AdvancedNamingConvention {
-    private static final int adult_Age = 18;
+    private static final int ADULT_AGE = 18;
+
     private int age;
     private int phoneNumber;
 
-    void callFriend() {
-        //some code
+    void callToFriend() {
+        // some code
     }
 
-    void callByNumber(int Number) {
-        //some code
+    void callByNumber(int number) {
+        // some code
     }
 }
--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -2,13 +2,17 @@ package school.mjc.stage0.module3.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int initialValue = 0;
-        int maxLimit = 2000;
-        int minValue = 1000;
-        int firstValue = 4000;
-        int secondValue = 3000;
-        int sixthValue = 5000;
-        int seventhValue = 7000;
-        int largeValue = 90000;
+        int variable = 0;
+        int variable2 = 2000;
+        int value = 1000;
+        int variable3 = 4000;
+        int value2 = 3000;
+        int variables = 5000;
+        variables = 7000;
+        int value3 = 90000;
+        int intValue = 100000;
+        int myInteger = 1000000;
+        int myVar = 1234567;
+        int myInteger2 = 1234567;
     }
 }
